b7806fc89da0948e2b878e798d968f764759c9c4
Eugene <eugene97@connect.hku.hk>
2021-09-09 23:27:24 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+
+const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+
+const schema = makeExecutableSchema({
+  typeDefs: typesArray,
+  resolvers: {
+    Query: {
+      products: async (parent) => {
+        console.log("Getting the products...");
+        const product = await Promise.resolve(parent.products);
+        return product;
+      },
+      orders: (parent) => {
+        console.log("Getting orders");
+        return parent.orders;
+      },
+    },
+  },
+});
+
+const root = {
+  products: require("./products/products.model"),
+  orders: require("./orders/orders.model"),
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(3000, () => {
+  console.log("Running GraphQL server...");
+});
